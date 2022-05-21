16cf92cfc8905966e17b3d3cbe5f785d7442979f
jlTsZGpw <lordwerneo@gmail.com>
2022-05-21 14:17:54 +0300

    Created currency model

--- app/converter/models.py
+++ app/converter/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from datetime import datetime, timezone
+
 
 # Create your models here.
+class Currency(models.Model):
+    currency_code = models.IntegerField(primary_key=True, unique=True, )
+    currency_code_l = models.TextField(max_length=3, unique=True, validators=[MinLengthValidator(3),
+                                                                              MaxLengthValidator(3)])
+    units = models.IntegerField(blank=False)
+    amount = models.FloatField(blank=False)
+    update_time = models.DateTimeField(default=datetime.now, blank=False)
+
+    def __str__(self):
+        return f'{self.units} of {self.currency_code_l} costs {self.amount} UAH'
--- app/converter/urls.py
+++ app/converter/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.urls import path
 from .views import index
 
+app_name = 'converter'
+
 urlpatterns = [
     path('', index, name='index')
 ]
